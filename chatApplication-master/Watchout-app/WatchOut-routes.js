81ade99c231ea8273b629cae8055a85f5ae8407a
0crypto0 <0crypto0@gmail.com>
2016-09-17 17:10:55 +0300

    a

--- chatApplication-master/Watchout-app/WatchOut-routes.js
+++ chatApplication-master/Watchout-app/WatchOut-routes.js
@@ -3,6 +3,10 @@ var _ = require('underscore');
 var Online = require(' ./../app/models/online');
 var async = require('async');
 var dateFormat = require('dateformat');
+var Client = require('./models/client');
+var SuspeciousWord = require('./models/suspeciouswords');
+var Suspect = require('./models/suspects');
+var Message = require('./../app/models/message');
 
 var sessionTime = 10000; // 10 seconds
 
@@ -22,10 +26,10 @@ module.exports = function(app, passport, io) {
 
 	// process login
 	app.post('/WatchOut-login', function(req, res, next) {
-		passport.authenticate('WatchOut-login', function(err, user, info) {
+		passport.authenticate('WatchOut-login', function(err, client, info) {
 			if (err) return next(err);
-			if (!user) return res.redirect('/WatchOut-login');
-			req.logIn(user, function(err) {
+			if (!client) return res.redirect('/WatchOut-login');
+			req.logIn(client, function(err) {
 				if (err) return next(err);
 				var redirect_to = req.session.redirect_to || '/WatchOut-profile';
 				return res.redirect(redirect_to);
@@ -39,45 +43,29 @@ module.exports = function(app, passport, io) {
 	});
 
 	// process signup
-	app.post('/signup', function(req, res, next) {
-		passport.authenticate('singup', function(err, user, info) {
+	app.post('/WatchOut-signup', function(req, res, next) {
+		passport.authenticate('WatchOut-signup', function(err, client, info) {
 			if (err) return next(err);
-			if (!user) return res.redirect('/signup');
-			req.logIn(user, function(err) {
+			if (!client) return res.redirect('/WatchOut-signup');
+			req.logIn(client, function(err) {
 				if (err) return(next);
-				return res.redirect('/profile');
+				return res.redirect('/WatchOut-profile');
 			});
 		})(req, res, next);
 	});
 
 	// profile page
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', { user: req.user });
+	app.get('/WatchOut-profile', isLoggedIn, function(req, res) {
+		res.render('WatchOut-profile.ejs', { client: req.client });
 	});
 
 	// logout
-	app.get('/logout', function(req, res) {
+	app.get('/WatchOut-logout', function(req, res) {
 		delete req.session.redirect_to;
 		req.logout();
 		res.redirect('/');	// redirect to home page
 	});
 
-	// chat page
-	app.get('/chat', isLoggedIn, function(req, res) {
-		var user = req.body.user;
-		// if (user){
-		// 	console.log("test req.body.user: "+user.username)
-		// }
-		res.render('chat.ejs', { userId: req.user._id });
-	});
-
-	// process message which sent from the client
-	app.post('/messages', function(req, res, next) {
-		var message = req.body.message;
-		var userId = req.body.userId;
-		createMessage(userId, message, res, next);
-		
-	});
 
 	// get all messages from the database
 	app.get('/messages', function(req, res, next) {
@@ -89,30 +77,30 @@ module.exports = function(app, passport, io) {
 	});
 
 	// get all the user who is online
-	app.get('/userOnline', function(req, res, next) {
-		userOnline(function(data) {
+	app.get('/clientOnline', function(req, res, next) {
+		clientOnline(function(data) {
 			res.json(data);
 		});
 	});
 
 	// update user's status (online or offline)
 	io.on('connection', function(socket){
-		var user;
+		var client;
 		var isOnline = false;
 		var timeOutId = 0;
 
 		socket.on("i-am-online", function(data) {
 			if (data) {
-				userId = data.userId;
-				User.findById(userId, function(err, user) {
+				clientId = data.clientId;
+				Client.findById(clientId, function(err, client) {
 					if (err) return callback(err);
-					if (user) {
+					if (client) {
 						if (!isOnline) {
-							var index = usersOnline.indexOf(user.username);
+							var index = clientsOnline.indexOf(client.username);
 							if (index <= -1) {
-								usersOnline.push(user.username);
+								clientsOnline.push(client.username);
 							}
-							io.emit("onlines", usersOnline);
+							io.emit("onlines", clientsOnline);
 							// isOnline = true;
 							// setUserOnline(user);
 						}
@@ -123,15 +111,15 @@ module.exports = function(app, passport, io) {
 						} 
 
 						timeOutId = setTimeout(function() {
-							var index = usersOnline.indexOf(user.username);
+							var index = clientsOnline.indexOf(client.username);
 							if (index > -1) {
-								usersOnline.splice(index, 1);
+								clientsOnline.splice(index, 1);
 							}
-							io.emit("onlines", usersOnline);
+							io.emit("onlines", clientsOnline);
 							// setUserOffline(user);
 							// isOnline = false;
 						}, sessionTime);
-						console.log("test usersOnline: "+usersOnline);
+						console.log("test clientsOnline: "+clientsOnline);
 					}
 				});
 			}
@@ -142,131 +130,50 @@ module.exports = function(app, passport, io) {
 	function isLoggedIn(req, res, callback) {
 		if (req.isAuthenticated()) return callback();
 		req.session.redirect_to = req.originalUrl;
-		res.redirect('/login');
+		res.redirect('/WatchOut-login');
 	}
 
 	// find users who are online
-	function userOnline(callback) {
-		var userOnline = [];
+	function clientsOnline(callback) {
+		var clientOnline = [];
 		Online.find({}, function(err, data) {
 			for (var i in data) {
 				if (data[i].isOnline) {
-					userOnline.push(data[i]);
+					clientOnline.push(data[i]);
 				}
 			}
-			callback(userOnline);
+			callback(clientOnline);
 		})
 	}
 
-	// get all message from db and format its date
-	function findUserMessage(messages, callback) {
-		var allMessages = [];
-
-		async.eachSeries(messages, function iterator(elt, cb) {
-			// var user_id = mongoose.Types.ObjectId(elt.user_id);
-			var user_id = elt.user_id;
-			message = JSON.parse(JSON.stringify(elt));
-
-			var date = dateFormat(message.create_at, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-			message.dateFomat = date;
-			User.findById(user_id, function(err, user) {
-				if (err) return callback(err);
-				message.username = user.username;
-				allMessages.push(message);
-				cb();
-			});
-		}, function done() {
-			callback(allMessages)
-		});
-	}
-
 	// update user's status and send to all clients
-	function setUserStatus(user, isOnline) {
-		Online.findOne({'username': user.username}, function(err, data) {
+	function setClientStatus(client, isOnline) {
+		Online.findOne({'username': client.username}, function(err, data) {
 			if (err) throw err;
 			if (data) {
 				data.isOnline = isOnline;
 				data.save();
 			} else { //create new user online if the user is not exist
-				var newUserOnline = new Online();
-				newUserOnline.username = user.username;
+				var newClientOnline = new Online();
+				newUserOnline.username = client.username;
 				newUserOnline.isOnline = isOnline;
 				newUserOnline.save();
 			}
 			// send all users who is online for clients
-			userOnline(function(data) {
+			clientOnline(function(data) {
 				io.emit("onlines", data);
 			});
 		});
 	}
 
 	// set user is online
-	function setUserOnline(user) {
-		setUserStatus(user, true);
+	function setClientOnline(client	) {
+		setClientStatus(client, true);
 	}
 
 	// set user is offline
-	function setUserOffline(user) {
-		setUserStatus(user, false);
+	function setClientOffline(client) {
+		setClientStatus(client, false);
 	}
 
-	function createMessage(userId, content, res, callback) {
-		User.findById(userId, function(err, user) {
-			if (err) throw err;
-			if (user) {
-				initCounter(function() {
-					getNextSequence("messageId", function(counter) {
-						var messageModel = new Message();
-						messageModel._id = counter.seq;
-						messageModel.user_id = userId;
-						messageModel.content = content;
-						messageModel.save(function(err, result) {
-							if (!err) {
-								Message.find({}).sort('-create_at').exec(function(err, messages) {
-									findUserMessage(messages, function(data) {
-										io.emit('message', { messages: data });
-									});			
-								});
-								res.send("Message Sent!");
-							} else {
-								res.send("some errors occured!");
-							}
-						});
-					});
-				});
-			}
-		});
-	}
-
-	// if the counter is not exist, we will create a counter
-	function initCounter(callback) {
-		Counter.findOne({'_id': 'messageId'}, function(err, done) {
-			if (err) throw err;
-			if (!done) {
-				var messageCounter = new Counter();
-				messageCounter._id = "messageId";
-				messageCounter.seq = 0;
-				messageCounter.save(function(err) {
-					if (err) throw err;
-					callback();
-				});
-			} else {
-				callback();
-			}
-		});
-	}
-
-	function getNextSequence(name, callback) {
-		Counter.findOneAndUpdate(
-			{ _id: name },
-			{ $inc: { seq: 1 } },
-			{ new: true, upsert: true },
-			function(err, done) {
-				if (err) throw err;
-				if (done) {
-					callback(done);
-				}
-			}
-		);
-	}	
 };
